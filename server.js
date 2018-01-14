98801f3ae302ad9de20213996560f73d104175f7
Keller Beckstead <kellerjbeckstead@gmail.com>
2018-01-14 00:02:14 -0700

    just a few changes

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 
-let exp = express();
 const bP = bodyParser();
 
+const index = require('./routes/index');
+const tasks = require('./routes/tasks');
+
+const port = 3000;
+
+const app = express();
+
+
+//View Engine
+app.set('veiws', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
